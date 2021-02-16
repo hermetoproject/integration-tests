b2e465b91a6a272540c77d4dde1e317773ed700b
Adam Cmiel <acmiel@redhat.com>
2021-02-16 13:37:28 +0100

    Add a subpackage inside the local dependency
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/cachito-testing/some-module"
+	"github.com/cachito-testing/some-module/some-package"
 )
 
 
 func main() {
 	fmt.Println("Hello, local dependencies.")
 	some_module.Hi()
+	some_package.Hi()
 }
