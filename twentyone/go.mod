e5c2df30b087c6524b21afa983afc862b4a88177
Taylor Madore <tmadore@redhat.com>
2024-03-21 16:19:59 -0400

    add twentyone module
    This module requires minimum go version 1.21
    
    Signed-off-by: Taylor Madore <tmadore@redhat.com>

---
+++ twentyone/go.mod
@@ -0,0 +1,3 @@
+module github.com/cachito-testing/cachi2-gomod/twentyone
+
+go 1.21
---
+++ twentyone/twentyone.go
@@ -0,0 +1,7 @@
+package twentyone
+
+import "fmt"
+
+func PrintTwentyOne() {
+    fmt.Println("The cachi2-gomod/twentyone module requires minimum go version 1.21")
+}
