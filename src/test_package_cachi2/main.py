37ade8b171a316a9b909fcf0ce8c90817b487d7a
Alexey Ovchinnikov <aovchinn@redhat.com>
2025-03-07 14:36:33 -0600

    Test for Rust extensions for Python
    This patch introduces necessary components for testing
    together cargo, pip and rpm PMs.
    
    Signed-off-by: Alexey Ovchinnikov <aovchinn@redhat.com>

--- src/test_package_cachi2/main.py
+++ src/test_package_cachi2/main.py
@@ -3,16 +3,7 @@
 A simple script for checking pre-installed packages provided by Cachi2
 """
 
-import io
-import requests
-from dockerfile_parse import DockerfileParser
-
-
-if __name__ == '__main__':
-    dfp = DockerfileParser(fileobj=io.BytesIO())
-    containerfile_url = "https://raw.githubusercontent.com/containerbuildsystem/cachi2/e793f7c1c446994a353769a89c8395a3ed0639bc/Containerfile"
-    r = requests.get(containerfile_url, allow_redirects=True)
-    dfp.content = r.content
-
-    # Print parent image
-    print(dfp.parent_images[0])
+# This will fail if the package is missing.
+# If this does not fail then we can assume that the package
+# was successfully built and installed.
+from cryptography.fernet import Fernet
