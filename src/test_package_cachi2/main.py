858275075f064ca4a17d2f4225daa68c77cb534d
Michal Šoltis <msoltis@redhat.com>
2026-06-01 11:08:52 +0200

    Use uv package manager and drop cachi2 historicisms
    The requirements-txt file is now generated directly from pyproject.toml.
    
    Signed-off-by: Michal Šoltis <msoltis@redhat.com>

--- src/test_package_cachi2/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-A simple script for checking pre-installed packages provided by Cachi2
-"""
-
-# This will fail if the package is missing.
-# If this does not fail then we can assume that the package
-# was successfully built and installed.
-from cryptography.fernet import Fernet
